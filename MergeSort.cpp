318b9eae3febe9578d444d160139654a8234bc2f
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-04-15 09:36:45 +0600

    while loop 3 for sorting

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -53,4 +53,10 @@ void mergesort(int low, int high) { // Step 1
         k++; 
     }
 
-    
+    while (i <= mid) { // Step 4.f
+        B[k] = arr[i]; 
+        i++; 
+        k++; 
+    }
+
+   
