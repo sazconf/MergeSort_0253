d1a174cee91456e364e4810bb05c40a505cbde01
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-04-15 09:32:39 +0600

    input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,3 +5,17 @@ using namespace std;
 int arr[20], B[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if (n <= 20) break;
+        else cout << "Please enter a number less than or equal to 20." << endl;
+    }
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
